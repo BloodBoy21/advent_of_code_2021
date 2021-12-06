54b490770e50e5760bbddc569531029cda72d253
Alan B <death1027@outlook.com>
2021-12-05 19:15:24 -0600

    Day 5: second part

--- 5/main.py
+++ 5/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import math
 inputData = []
 
 with open('input.txt','r') as f:
@@ -6,15 +8,25 @@ with open('input.txt','r') as f:
     inputData = [[[int(x) for x in num.split(',')] for num in line] for line in inputData]
 
 
+def getAngle(cloud):
+    try:
+        x1,x2 = cloud[0][1],cloud[1][1]
+        y1,y2 = cloud[0][0],cloud[1][0]
+        m = (y2-y1)/(x2-x1)
+        return  math.degrees(np.arctan(m))
+    except ZeroDivisionError:
+        return 0
+
 class Cloud():
     def __init__(self,coordinates):
         self.coordinates = coordinates
         self.points = []
         self.commountPoint = 0
         self.endPoint = 0
-        self.__createPoints()
-    def __createPoints(self):
-        self.__orderCoordinates()
+        self.createPoints()
+
+    def createPoints(self):
+        self.orderCoordinates()
         firstCoordinate = self.coordinates[0]
         secondCoordinate = self.coordinates[1]
         for i in range(2):
@@ -25,7 +37,7 @@ class Cloud():
                     newPoint[self.commountPoint] = firstCoordinate[self.commountPoint]
                     self.points.append(newPoint)
 
-    def __orderCoordinates(self):
+    def orderCoordinates(self):
         firstCoordinate = self.coordinates[0]
         secondCoordinate = self.coordinates[1]
         for i in range(2):
@@ -37,6 +49,38 @@ class Cloud():
                 p1,p2 = (p2,p1) if p1>p2 else (p1,p2)
                 self.coordinates[0][i]=p1
                 self.coordinates[1][i]=p2
+
+class DiagonalCloud(Cloud):
+    def __init__(self,coordinates):
+        Cloud.__init__(self,coordinates)
+    
+    def createPoints(self):
+        self.orderCoordinates()
+        method = {45.0:self.__normal,-45.0:self.__inverted}[getAngle(self.coordinates)]
+        method()
+
+    def __normal(self):
+        x1,x2 = self.coordinates[0][0],self.coordinates[1][0]
+        y1,y2 = self.coordinates[0][1],self.coordinates[1][1]
+        for y in range (y1,y2+1):
+            coordinates = [x1,y] 
+            self.points.append(coordinates)
+            x1+=1
+    def __inverted(self):
+        x1,x2 = self.coordinates[0][0],self.coordinates[1][0]
+        y1,y2 = self.coordinates[0][1],self.coordinates[1][1]
+        for y in range (y2,y1+1):
+            coordinates = [x2,y] 
+            self.points.append(coordinates)
+            x2-=1
+
+    def orderCoordinates(self):
+        firstCoordinate = self.coordinates[0]
+        secondCoordinate = self.coordinates[1]
+        x1,x2 = firstCoordinate[0],secondCoordinate[0]
+        if x1>x2:
+            self.coordinates = [secondCoordinate,firstCoordinate] 
+
 
 class HashMap():
     def __init__(self,data={}):
@@ -56,13 +100,22 @@ class HashMap():
                 count += 1
         return count
 
+def isDiagonal(cloud):
+    angle = abs(getAngle(cloud))
+    if angle == 45.0:
+        return True
+    return False
+
 def filterClouds(clouds):
     newClouds = []
     for cloud in clouds:
         x1,x2 = cloud[0][1],cloud[1][1]
         y1,y2 = cloud[0][0],cloud[1][0]
-        if x1 == x2 or y1 == y2:
+        if isDiagonal(cloud):
+            newClouds.append(DiagonalCloud(cloud))
+        elif x1 == x2 or y1 == y2:
             newClouds.append(Cloud(cloud))
+
     return newClouds
 
 def getWarningPoints(cloudsData):
@@ -73,5 +126,5 @@ def getWarningPoints(cloudsData):
             points.add(point,1)
     print(points.filterBySizeUp(2))
 
-getWarningPoints(inputData)
- 
+if __name__ == '__main__':
+    getWarningPoints(inputData)
